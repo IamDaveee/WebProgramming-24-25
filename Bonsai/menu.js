53af89ddf32c49c95a59f282a420e3254c4f7134
Dave <121976848+IamDaveee@users.noreply.github.com>
2025-02-18 10:34:36 +0100

    Add files via upload

---
+++ Bonsai/menu.js
@@ -0,0 +1,7 @@
+
+    let menu =
+        '<li><a href="index.html">Bevezető</a></li>' +
+        '<li><a href="tortenet.html">Történet</a></li>'+
+        '<li><a href="kepgaleria.html">Képgaléria</a></li>'+
+        '<li><a href="linkek.html">Linkek</a></li>';
+    document.getElementById("menu").innerHTML = menu;
